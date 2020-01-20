ad46ccbf1eacb0b1fa7d9d1c76f1035a5f67947e
LuoChengIo <374857968@qq.com>
2020-01-20 17:36:10 +0800

    Create ConnectItem.vue

---
+++ demo/src/ConnectItem.vue
@@ -0,0 +1,24 @@
+<!--  -->
+<template>
+  <div class="connect-item">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConnectItem',
+  components: {},
+  data() {
+    return {
+    }
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
